f6ebb4474fe336bc60513de9214a46b507098686
Yousif Aldolaijan <yad999@gmail.com>
2019-06-23 22:35:30 +0300

    First Commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    float forceScale = 4;
+
+    // Start is called before the first frame update
+    void Start() {
+        Physics2D.gravity = Vector2.zero;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.D)) {
+            //if (rb.velocity.normalized != Vector2.right) {
+            //    rb.velocity = Vector2.zero;
+            //}
+            rb.AddForce(Vector2.right * forceScale, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            //if (rb.velocity.normalized != Vector2.left) {
+            //    rb.velocity = Vector2.zero;
+            //}
+            rb.AddForce(Vector2.left * forceScale, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W)) {
+            //if (rb.velocity.normalized != Vector2.up) {
+            //    rb.velocity = Vector2.zero;
+            //}
+            rb.AddForce(Vector2.up * forceScale, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            //if (rb.velocity.normalized != Vector2.down) {
+            //    rb.velocity = Vector2.zero;
+            //}
+            rb.AddForce(Vector2.down * forceScale, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.velocity = Vector2.zero;
+        }
+
+        Debug.Log(rb.velocity);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.GetComponent<expander>()) {
+            Debug.Log("DEAD");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/expander.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class expander : MonoBehaviour
+{
+
+    CircleCollider2D collider;
+    public Vector3 exapdnVector;
+    public float expandFactor = 1.1f;
+    float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<CircleCollider2D>();
+        exapdnVector = new Vector3(0.1f, 0.1f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.localScale += exapdnVector;
+        time += Time.deltaTime;
+        if (time >= 0.5f) {
+            transform.localScale *= expandFactor;
+            time = 0;
+        }
+    }
+}
